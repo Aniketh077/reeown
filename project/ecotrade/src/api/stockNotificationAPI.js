47f1444be73428f1f014c8b3f40f7dd19c235df7
Aniketh Vadthya <aniketh0701@gmail.com>
2025-11-10 17:11:26 +0530

    Updated stockNotificationAPI.js

--- project/ecotrade/src/api/stockNotificationAPI.js
+++ project/ecotrade/src/api/stockNotificationAPI.js
@@ -2,7 +2,7 @@ import axios from './axios';
 
 export const stockNotificationAPI = {
   requestNotification: async (productId, email) => {
-    const response = await axios.post('/stock-notifications/request', {
+    const response = await axios.post('/api/stock-notifications/request', {
       productId,
       email
     });
